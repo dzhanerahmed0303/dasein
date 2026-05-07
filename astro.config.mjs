656f3d8f98c70c0a59483f7583b49e9db6751bd8
dzhanerahmed0303 <dzhaner.ahmedd@gmail.com>
2026-05-07 21:15:34 +0200

    server - serverless error opgelost

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 import { defineConfig, fontProviders } from "astro/config";
-import vercel from "@astrojs/vercel";
+
 import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
@@ -12,8 +12,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://dzhaner.vercel.app",
-  output: "server",
-  adapter: vercel(),
+  output: "static",
   integrations: [mdx(), sitemap(), pagefind(), icon()],
 
   vite: {
